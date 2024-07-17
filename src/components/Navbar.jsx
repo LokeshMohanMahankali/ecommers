e697e239b84f46c40f0b67aade3ff2adf440c89f
Lokesh Mohan Mahankali <lokeshmahankali@gmail.com>
2024-07-18 00:01:31 +0530

    header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,85 @@
 import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const Navbar = () => {
-  const Container = styled.div`
-    background-color: skyblue;
-  `;
+  const Container = styled.div``;
 
   const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
   `;
 
-  const Left = styled.div``;
-  const Center = styled.div``;
-  const Right = styled.div``;
+  // left header part
+  const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+  `;
+  const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+  `;
+  const Searchcontainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+  `;
+
+  const Input = styled.input`
+    border: none;
+  `;
+
+  // Center header part
+  const Center = styled.div`
+    flex: 1;
+    text-align: center;
+  `;
+
+  const Logo = styled.h1`
+    font-weight: bold;
+  `;
+  // Right header part
+  const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  `;
+
+  const Menuitems = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+  `;
 
   return (
     <Container>
       <Wrapper>
-        <Left>Left</Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Left>
+          <Language>English</Language>
+          <Searchcontainer>
+            <Input />
+            <SearchIcon style={{ color: "gray", fontSize: "14px" }} />
+          </Searchcontainer>
+        </Left>
+        <Center>
+          <Logo>LAMA</Logo>
+        </Center>
+        <Right>
+          <Menuitems>Register</Menuitems>
+          <Menuitems>Sign-In</Menuitems>
+          <Menuitems>
+            <Badge color="secondary" badgeContent={5}>
+              <ShoppingCartIcon />
+            </Badge>
+          </Menuitems>
+        </Right>
       </Wrapper>
     </Container>
   );
