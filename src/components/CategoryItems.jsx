758eac669c111ba978e5d6a0ff2a795ebf5be003
Lokesh Mohan Mahankali <lokeshmahankali@gmail.com>
2024-07-20 16:19:36 +0530

    slider and category

---
+++ src/components/CategoryItems.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CategoryItems = () => {
+  return <div>Categortyitems</div>;
+};
+
+export default CategoryItems;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,15 @@
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import styled from "styled-components";
+import { sliderItems } from "../pages/data";
+import { useState } from "react";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -22,21 +24,26 @@ const Arrow = styled.div`
   top: 0;
   bottom: 0;
   margin: auto;
+  cursor: pointer;
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
-  cursor: pointer;
   opacity: 0.5;
+  z-index: 10;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.slideindex * -100}vw);
+  transition: all 1.5s ease;
 `;
 
 const Slide = styled.div`
   display: flex;
   align-item: center;
-  height: 100vw;
-  width: 100vh;
+  height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.bg};
 `;
 const ImgContainer = styled.div`
   flex: 1;
@@ -44,40 +51,61 @@ const ImgContainer = styled.div`
 `;
 
 const Image = styled.img`
-  height: 80%;
+  height: 100%;
 `;
 
 const Infocontainer = styled.div`
   flex: 1;
-  padding: 50px;
-  background-color: white;
+  padding-top: 250px;
 `;
 
-const Title = styled.h1``;
-const Desc = styled.p1``;
-const Button = styled.button``;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 22px;
+`;
+const Desc = styled.p`
+  font-size: 20px;
+  margin: 50px 0px;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  border: solid 2px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+  const [slideindex, setSlideindex] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideindex(slideindex > 0 ? slideindex - 1 : 2);
+    } else {
+      setSlideindex(slideindex < 2 ? slideindex + 1 : 0);
+    }
+  };
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftIcon />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImgContainer>
-            <Image src="https://i.ibb.co/DG69bQ4/2.png" />
-          </ImgContainer>
-          <Infocontainer>
-            <Title>Summer SAL</Title>
-            <Desc>
-              Don'T COMPOMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
-            </Desc>
-            <Button>SHOW NOW</Button>
-          </Infocontainer>
-        </Slide>
+      <Wrapper slideindex={slideindex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <Infocontainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </Infocontainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightIcon />
       </Arrow>
     </Container>
